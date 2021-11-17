c3ac39ecc5887ef655d52e164c5f188122a9da0d
Marco Guido <guido-torres@hotmail.com>
2021-11-17 14:50:03 -0600

    fixed product test cases #4 and #5

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -34,15 +34,15 @@ try {
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
-  var result = product(2, 2);
-  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
+  var result = product(2);
+  if (result !== 2) throw new Error('Expected product(2) to be 2. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
-  var result = product(0);
-  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
+  var result = product();
+  if (result !== 0) throw new Error('Expected product(0) to be 0. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
