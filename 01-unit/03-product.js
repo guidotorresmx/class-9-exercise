84018baf6f0b198dd1a4ccc951abae6a6d4db978
Marco Guido <guido-torres@hotmail.com>
2021-11-17 14:49:40 -0600

    updated product function and test cases

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,8 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(a=0, b=1) {
+  return a*b;
 }
 
 // ==================================================
@@ -20,21 +20,29 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
+  var result = product(2, 0);
+  if (result !== 0) throw new Error('Expected product(2, 0) to be 0. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should ignore additional numbers.
+  var result = product(2, 2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = product(2, 2);
+  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = product(0);
+  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
 
   console.log('All tests passed successfully.');
 
